31976c31d76574ab53db9b3d658b243cd0212cce
wonjunYou <youwjune@gmail.com>
2023-12-28 20:03:43 +0900

    feat : business layer test - 재고 차감 로직

---
+++ src/main/java/sample/cafekiosk/spring/domain/stock/Stock.java
@@ -0,0 +1,49 @@
+package sample.cafekiosk.spring.domain.stock;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Stock {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String productNumber;
+
+    private int quantity;
+
+    @Builder
+    public Stock(String productNumber, int quantity) {
+        this.productNumber = productNumber;
+        this.quantity = quantity;
+    }
+
+    public static Stock create(String number, int quantity) {
+        return Stock.builder()
+                .productNumber(number)
+                .quantity(quantity)
+                .build();
+    }
+
+    public boolean isQuantityLessThan(int quantity) {
+        return this.quantity < quantity;
+    }
+
+    public void deductQuantity(int quantity) {
+        if (isQuantityLessThan(quantity)) {
+            throw new IllegalArgumentException("차감할 재고 수량이 없습니다.");
+        }
+
+        this.quantity -= quantity;
+    }
+}
